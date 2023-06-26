8d92d7d4b29b3781ea806435e3baed3c86d9c5e2
carloseugventurin <carloseugventurin@gmail.com>
2023-06-26 09:00:51 -0300

    aa

--- trabalho.py
+++ trabalho.py
@@ -3,4 +3,23 @@ import matplotlib.pyplot as plt
 
 bd = pd.read_csv("dataset.csv")
 
-plt.bar(bd['certificate'].value_counts().index, bd['certificate'].value_counts().values);
+print(bd['run_time'][0])
+print(bd['run_time'][1])
+
+
+tempomin = []
+for k in range(len(bd)):
+    a = bd['run_time'][k].replace('m', 'h').split('h')
+    soma = 0
+    for i in range(len(a)):
+        if a[i] == 'Not Available':
+            tempomin.append('Not Available')
+        else:
+            if i == 0:
+                soma += (60*int(a[i]))
+            if i == 1 and a[i] != '':
+                soma += int(a[i])
+    if a[0] != 'Not Available':
+        tempomin.append(soma)
+bd['run_time'] = tempomin
+print(bd.head())
